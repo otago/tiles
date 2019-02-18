7871cc9b29eb96c3cef0dfd77725cc9a2c943ac1
torleif <torleifw@gmail.com>
2019-02-19 09:16:05 +1300

    Update TileElement.php

--- src/Elements/TileElement.php
+++ src/Elements/TileElement.php
@@ -12,37 +12,35 @@ use SilverStripe\Forms\TextField;
 
 class TileElement extends BaseElement {
 
-	private static $singular_name = 'Tile';
-	private static $plural_name = 'Tiles';
-	private static $description = 'creates a grid of tiles';
-	private static $db = [
-		'Rows' => 'Int'
-	];
-	private static $has_many = [
-		'Tiles' => Tile::class
-	];
-	private static $owns = [
-		'Tiles'
-	];
+    private static $singular_name = 'Tile';
+    private static $plural_name = 'Tiles';
+    private static $description = 'creates a grid of tiles';
+    private static $db = [
+        'Rows' => 'Int'
+    ];
+    private static $has_many = [
+        'Tiles' => Tile::class
+    ];
+    private static $owns = [
+        'Tiles'
+    ];
     private static $table_name = 'TileElement';
 
-	public function getCMSFields() {
-		$fields = FieldList::create();
-		$fields->push(new TabSet("Root", $mainTab = new Tab("Main")));
+    public function getCMSFields() {
+        $fields = parent::getCMSFields();
+        $fields->removeByName(['Rows', 'Tiles']);
 
-		$fields->addFieldToTab('Root.Main', TextField::create('Title', 'Title'));
-		
-		$tilefield = TileField::create('Tiles', 'Tiles', $this->Tiles(), null, $this);
-		if (!$this->ID) {
-			$tilefield->setDisabled(true);
-			$tilefield->setDescription('Please save to begin editing');
-		}
-		$fields->addFieldToTab('Root.Main', $tilefield);
-		return $fields;
-	}
+        $tilefield = TileField::create('Tiles', 'Tiles', $this->Tiles(), null, $this);
+        if (!$this->ID) {
+            $tilefield->setDisabled(true);
+            $tilefield->setDescription('Please save to begin editing');
+        }
+        $fields->addFieldToTab('Root.Main', $tilefield);
+        return $fields;
+    }
 
-	public function getType() {
-		return 'Tiles';
-	}
+    public function getType() {
+        return 'Tiles';
+    }
 
 }

6c380f24f18cc62c96423b7d108d3a792623ff80
torleif <torleifw@gmail.com>
2019-03-18 16:27:51 +1300

    Update LinkTile.php

--- src/Models/LinkTile.php
+++ src/Models/LinkTile.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace OP\Models;
+
 use SilverStripe\Assets\Image;
 use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\AssetAdmin\Forms\UploadField;
@@ -8,6 +9,8 @@ use SilverStripe\Control\Controller;
 use SilverStripe\Forms\TreeDropdownField;
 use SilverStripe\Forms\TextField;
 use SilverStripe\Control\Director;
+use SilverStripe\ORM\FieldType\DBField;
+use SilverStripe\ORM\FieldType\DBHTMLText;
 
 /**
  * A tile the holds an image, and an external or internal link to resource
@@ -15,59 +18,57 @@ use SilverStripe\Control\Director;
 class LinkTile extends Tile {
 
     private static $table_name = 'LinkTile';
-	private static $singular_name = "Link Tile";
-	protected static $allowed_sizes = [
-		'1x1'
-	];
-	private static $db = [
-		'Title' => 'Text', // text in the content field
-		'URL' => 'Text'
-	];
-	private static $has_one = [
-		'Image' => Image::class,
-		'Tree' => SiteTree::class
-	];
+    private static $singular_name = "Link Tile";
+    protected static $allowed_sizes = [
+        '1x1'
+    ];
+    private static $db = [
+        'Title' => 'Text', // text in the content field
+        'URL' => 'Text'
+    ];
+    private static $has_one = [
+        'Image' => Image::class,
+        'Tree' => SiteTree::class
+    ];
 
-	public function getCMSFields() {
-		$fields = parent::getCMSFields();
-		$fields->addFieldToTab('Root.Main', TextField::create('URL', 'URL')->setDescription('For external links'));
-		$tree = new TreeDropdownField("TreeID", "Local page to link", SiteTree::class, 'ID', 'MenuTitle');
-		$tree->setDescription('Select the same item twice to clear. You may have to save before seeing pages in the dropdown.');
-		$fields->addFieldToTab('Root.Main', $tree);
+    public function getCMSFields() {
+        $fields = parent::getCMSFields();
+        $fields->addFieldToTab('Root.Main', TextField::create('URL', 'URL')->setDescription('For external links'));
+        $tree = new TreeDropdownField("TreeID", "Local page to link", SiteTree::class, 'ID', 'MenuTitle');
+        $tree->setDescription('Select the same item twice to clear. You may have to save before seeing pages in the dropdown.');
+        $fields->addFieldToTab('Root.Main', $tree);
 
-		$imageupload = new UploadField('Image', 'Upload Image');
-		$fields->addFieldToTab('Root.Main', $imageupload);
-		$imageupload->setAllowedFileCategories('image');
-		$imageupload->setFolderName('tiles/photo');
-		$imageupload->setAllowedMaxFileNumber(1);
-		$imageupload->setDescription('Image that will be displayed on the tile.');
-		$imageupload->setFolderName('widgets');
+        $imageupload = new UploadField('Image', 'Upload Image');
+        $fields->addFieldToTab('Root.Main', $imageupload);
+        $imageupload->setAllowedFileCategories('image');
+        $imageupload->setFolderName('tiles/photo');
+        $imageupload->setAllowedMaxFileNumber(1);
+        $imageupload->setDescription('Image that will be displayed on the tile.');
+        $imageupload->setFolderName('widgets');
 
-		return $fields;
-	}
+        return $fields;
+    }
 
-	public function Preview() {
-		if ($this->Content) {
-			return $this->Content;
-		}
-		if ($this->PageContent) {
-			return $this->PageContent;
-		}
-		if ($this->ImageID) {
-			if(!$this->Image()->SetRatioSize(230, 170)) {
-				return 'resize failed';
-			}
-			return $this->Image()->SetRatioSize(230, 170)->getTag();
-		}
-		return 'Pagination tile requires page content or an image.';
-	}
+    public function getPreviewImage() {
+        if (!$this->Image()->ThumbnailURL(230, 170)) {
+            return null;
+        }
+        return $this->Image()->ThumbnailURL(230, 170);
+    }
 
-	public function getLink() {
-		$tid = $this->TreeID ? : $this->treeid;
-		if ($tid && SiteTree::get()->byID($tid)) {
-			return Controller::join_links(Director::absoluteBaseURL(), SiteTree::get()->byID($tid)->Link());
-		}
-		return $this->URL ? : $this->url;
-	}
+    public function getPreviewContent() {
+        if (!$this->Tree()) {
+            return '';
+        }
+        return DBField::create_field(DBHTMLText::class, $this->Tree()->Title)->LimitCharacters(150);
+    }
+
+    public function getLink() {
+        $tid = $this->TreeID ?: $this->treeid;
+        if ($tid && SiteTree::get()->byID($tid)) {
+            return Controller::join_links(Director::absoluteBaseURL(), SiteTree::get()->byID($tid)->Link());
+        }
+        return $this->URL ?: $this->url;
+    }
 
 }

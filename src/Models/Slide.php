da77b4006b20b189a5ab012fdc5f479eeaaf7920
torleif <torleifw@gmail.com>
2019-03-28 09:44:03 +1300

    Update Slide.php

--- src/Models/Slide.php
+++ src/Models/Slide.php
@@ -7,39 +7,43 @@ use SilverStripe\ORM\DataObject;
 use SilverStripe\Forms\CheckboxField;
 use SilverStripe\Forms\TextField;
 
-
 /**
  * Slide - used for a tile with a collection of subobjects. Typically used in 
  * a slider with dots and a left/right button for navigation
  */
 class Slide extends DataObject {
+
     private static $table_name = 'TileImage';
-	private static $db = [
-		'VideoURL' => 'Boolean', 
-		'LinkURL' => 'Text', 
-		'Sort' => 'Int'
-	];
-	private static $has_one = [
-		'ParentTile' => Tile::class,
-		'Image' => Image::class
-	];
+    private static $db = [
+        'VideoURL' => 'Boolean',
+        'LinkURL' => 'Text',
+        'Sort' => 'Int'
+    ];
+    private static $has_one = [
+        'ParentTile' => Tile::class,
+        'Image' => Image::class
+    ];
     private static $summary_fields = [
-		'LinkURL',
-		'Thumbnail'
-	];
-	
-	public function getCMSFields() {
-		$fields = parent::getCMSFields();
-		
-		$fields->addFieldToTab('Root.Main', CheckboxField::create('VideoURL', 'Is Video'));
-		$fields->addFieldToTab('Root.Main',TextField::create('LinkURL', 'Link or Video resource'));
-		
-		return $fields;
-	}
-	
-	public function Thumbnail() {
-		if($this->Image()) {
-			return $this->Image()->CMSThumbnail();
-		}
-	}
+        'LinkURL',
+        'Thumbnail'
+    ];
+    private static $owns = [
+        'Image'
+    ];
+
+    public function getCMSFields() {
+        $fields = parent::getCMSFields();
+
+        $fields->addFieldToTab('Root.Main', CheckboxField::create('VideoURL', 'Is Video'));
+        $fields->addFieldToTab('Root.Main', TextField::create('LinkURL', 'Link or Video resource'));
+
+        return $fields;
+    }
+
+    public function Thumbnail() {
+        if ($this->Image()) {
+            return $this->Image()->CMSThumbnail();
+        }
+    }
+
 }

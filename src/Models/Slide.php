c91dc774ea9aad0dbb316a8ae91fa24a069d21e9
torleif <torleifw@gmail.com>
2021-03-11 12:00:23 +1300

    Update Slide.php

--- src/Models/Slide.php
+++ src/Models/Slide.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Slide - used for a tile with a collection of subobjects. Typically used in 
+ * a slider with dots and a left/right button for navigation
+ * @author torleif west <torleifw@op.ac.nz>
+ */
+
 namespace OP\Models;
 
 use SilverStripe\Assets\Image;
@@ -7,17 +13,15 @@ use SilverStripe\ORM\DataObject;
 use SilverStripe\Forms\CheckboxField;
 use SilverStripe\Forms\TextField;
 
-/**
- * Slide - used for a tile with a collection of subobjects. Typically used in 
- * a slider with dots and a left/right button for navigation
- */
-class Slide extends DataObject {
+class Slide extends DataObject
+{
 
     private static $table_name = 'TileImage';
     private static $db = [
         'VideoURL' => 'Boolean',
         'LinkURL' => 'Text',
-        'Sort' => 'Int'
+        'Sort' => 'Int',
+        'Hide' => 'Boolean'
     ];
     private static $has_one = [
         'ParentTile' => Tile::class,
@@ -33,19 +37,29 @@ class Slide extends DataObject {
 
     private static $default_sort = "Sort";
 
-    public function getCMSFields() {
+    /**
+     * CMS fields
+     * @return void
+     */
+    public function getCMSFields()
+    {
         $fields = parent::getCMSFields();
 
         $fields->addFieldToTab('Root.Main', CheckboxField::create('VideoURL', 'Is Video'));
         $fields->addFieldToTab('Root.Main', TextField::create('LinkURL', 'Link or Video resource'));
+        $fields->addFieldToTab('Root.Main', CheckboxField::create('Hide', 'Hide'));
 
         return $fields;
     }
 
-    public function Thumbnail() {
+    /**
+     * returns a smaller image
+     * @return Image
+     */
+    public function Thumbnail()
+    {
         if ($this->Image()) {
             return $this->Image()->CMSThumbnail();
         }
     }
-
 }
